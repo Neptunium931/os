6999ee53f965449f1c83cb1be9691b6ebfbd0004
neptumiun <tymothe.billerey@gmail.com>
2024-07-24 14:28:07 +0200

    add scrip for run OS

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/env bash
+
+set -e
+
+printf_line() {
+	line=$@
+	printf "%s " ${line[@]}
+	printf '\n'
+}
+
+runFloppy="qemu-system-x86_64 -drive file=src/floppy.img,index=0,if=floppy,format=raw"
+printf_line $runFloppy
+$($runFloppy)
