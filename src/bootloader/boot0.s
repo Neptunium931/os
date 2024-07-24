19b7e605f0f203ac739527f246d52269c73f7a12
neptumiun <tymothe.billerey@gmail.com>
2024-07-24 14:30:10 +0200

    feat(boot0): stage 0 bootloader fist verstion  finished
    disable interrupts
    set up registers
    set up stack
    reset floppy controller
    read boot1 for floppy
    check if read
    jump to boot1

--- src/bootloader/boot0.s
+++ src/bootloader/boot0.s
@@ -4,12 +4,47 @@
 .globl _start
 
 _start:
+# disable interrupts
+cli
 
-	movb al, 'H'
-	movb ah, 0x0e
-	int  0x10
+# set up segments registers
+movw ax, 0x0000
+movw ds, ax
+movw es, ax
+movw fs, ax
+movw gs, ax
+movw ss, ax
+
+# set up stack
+movw sp, 0x7c00
+
+# reset floppy controller
+movb ah, 0x0000
+int  0x13
+
+# read boot1
+movw bx, 0x1000
+movb ah, 0x02
+movb dl, 0x00
+movb dh, 0x00
+movb ch, 0x00
+movb cl, 0x02
+movb al, 0x02
+int  0x13
+
+cmpb al, 0x02
+jne  display_error
+
+movb al, 'R'
+movb ah, 0x0e
+int  0x10
 
 	jmp 0x1000
+
+display_error:
+	movb al, 'E'
+	movb ah, 0x0e
+	int  0x10
 
 	.     = _start + 510      #mov to 510th byte from 0 pos
 	.byte 0x55            #append boot signature
