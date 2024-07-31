369979400c434010a4eee7f8e1fddcd262785338
neptumiun <tymothe.billerey@gmail.com>
2024-07-31 11:23:53 +0200

    chore(run): add limit memory to 512 M

--- run.sh
+++ run.sh
@@ -8,6 +8,6 @@ printf_line() {
 	printf '\n'
 }
 
-runFloppy="qemu-system-x86_64 -drive file=src/floppy.img,index=0,if=floppy,format=raw"
+runFloppy="qemu-system-x86_64 -drive file=src/floppy.img,index=0,if=floppy,format=raw -m 512M"
 printf_line $runFloppy
 $($runFloppy)
